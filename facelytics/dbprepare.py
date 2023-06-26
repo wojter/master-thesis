30f70c3a4666fc620289d056931045b303bf8a78
wojter <wojtektro2@gmail.com>
2023-06-26 21:51:06 +0200

    Add df with identities with images count more than required

--- facelytics/dbprepare.py
+++ facelytics/dbprepare.py
@@ -3,6 +3,7 @@ import os
 
 total_images = 202_599
 total_identities = 10_177
+min_images_of_person = 8
 
 identity_path = "./CelebA/Anno/identity_CelebA.txt"
 
@@ -13,5 +14,10 @@ identities = pd.read_csv(identity_path, sep=" ", names=colnames, header=None)
 
 assert identities['file'].nunique() == total_images, "Import error, wrong total img number"
 assert identities['identity'].nunique() == total_identities, "Import error, wrong total identities number"
-# print(identities)
-print(identities.isna().any())
+assert identities.isna().values.any() == False, "NaN in table detected"
+
+temp = identities.drop("file", axis=1)
+temp = temp.groupby('identity').size().reset_index(name='occurences')
+temp = temp.drop(temp.index[temp["occurences"] < min_images_of_person])
+
+print(temp)
