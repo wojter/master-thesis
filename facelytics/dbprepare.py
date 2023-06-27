2500c13aedb315c27b106866274198635a7a79ea
wojter <wojtektro2@gmail.com>
2023-06-28 01:23:41 +0200

    Add filter identities function

--- facelytics/dbprepare.py
+++ facelytics/dbprepare.py
@@ -63,28 +63,45 @@ def get_face_detector(detector_backend):
     return face_detector
 
 
+def filter_cropp_rename_imgs(ident, ident_uniq):
+    images_naming = {}
+    total_identity_index = 0
+    for index, row in ident.iterrows():
+        # print(index, row.file, row.identity)
+        if row.identity in ident_uniq["identity"].values:
+            if row.identity in images_naming:
+                total_index, total_img = images_naming[row.identity]
+                total_img += 1
+                images_naming[row.identity] = (total_index, total_img)
+            else:
+                total_identity_index += 1
+                images_naming[row.identity] = (total_identity_index, 1)
+
+    print(len(images_naming))
+
+
 if __name__ == "__main__":
     args = args_inpust()
     min_images_of_person = int(args.num_img_of_identity)
     total_individuals_db = int(args.num_identities)
-    # identities, identities_unique = load_parse_identities_list()
 
-    face_detector = get_face_detector("mtcnn")
-    images = ["000001.jpg", "000002.jpg", "000003.jpg", "000004.jpg", "000005.jpg",
-              "000006.jpg", "000007.jpg", "000008.jpg", "000009.jpg", "000010.jpg",]
-    from time import time
-    multiple_faces = []
-    start = time()
-    for img_name in images:
-        img = os.path.join(imgs_path, img_name)
-        img = functions.load_image(img)
-        face_obj = FaceDetector.detect_faces(
-        face_detector, "mtcnn", img, align=True)
-        # from PIL import Image
-        # im = Image.fromarray(face_obj[0][0])
-        # im.save(os.path.join(result_imgs_path,img_name))
-        if(len(face_obj) != 1):
-            multiple_faces.append(img_name)
-        cv2.imwrite(os.path.join(result_imgs_path, img_name), face_obj[0][0])
+    identities, identities_unique = load_parse_identities_list()
 
-    print("detect 10 photo time", time()-start)
+    print(identities_unique)
+    filter_cropp_rename_imgs(identities, identities_unique)
+    # face_detector = get_face_detector("mtcnn")
+    # images = ["000001.jpg", "000002.jpg", "000003.jpg", "000004.jpg", "000005.jpg",
+    #           "000006.jpg", "000007.jpg", "000008.jpg", "000009.jpg", "000010.jpg",]
+    # from time import time
+    # multiple_faces = []
+    # start = time()
+    # for img_name in images:
+    #     img = os.path.join(imgs_path, img_name)
+    #     img = functions.load_image(img)
+    #     face_obj = FaceDetector.detect_faces(
+    #     face_detector, "mtcnn", img, align=True)
+    #     if(len(face_obj) != 1):
+    #         multiple_faces.append(img_name)
+    #     cv2.imwrite(os.path.join(result_imgs_path, img_name), face_obj[0][0])
+
+    # print("detect 10 photo time", time()-start)
