ae502b17f5dbb036d2326b6f99849c1ba4ff8a4e
wojter <wojtektro2@gmail.com>
2023-07-04 20:55:55 +0200

    Add runable test recognition

--- facelytics/test_recognition.py
+++ facelytics/test_recognition.py
@@ -1,7 +1,9 @@
 import os
 import argparse
 import time
-from deepface.DeepFace import represent
+
+from deepface.DeepFace import represent, build_model
+from deepface.commons import distance
 
 db_recognition_path = "CelebA\img_db_recognition"
 db_detection_path = "CelebA\img_db_detection"
@@ -34,6 +36,11 @@ def args_inpust():
     return parser.parse_args()
 
 
+def name_generator(ident_idx, img_idx):
+    name = f"{ident_idx:05d}_{img_idx:02d}.jpg"
+    return name
+
+
 def args_parser(args):
     model_name = args.model_name
     num_img_as_ref = int(args.num_img_identity_as_ref)
@@ -47,9 +54,33 @@ if __name__ == "__main__":
     model_name, num_img_as_ref, num_img_of_ident, num_ident = args_parser(args)
     if model_name not in models:
         raise ValueError("Wrong model name given. Pass correct name")
-    
+
     print("GO")
     start_recognition_time = time.time()
-    res = represent(os.path.join(db_recognition_path, "00001_03.jpg"), model_name=model_name, detector_backend="skip")
-    print(res)
+    threshold = distance.findThreshold(model_name, "cosine")
+
+    for k in range(1, 11):
+        person_ident = []
+        for i in range(1, 12):
+            if i <= 3:
+                res = represent(os.path.join(db_recognition_path, name_generator(
+                    1, i)), model_name=model_name, detector_backend="skip")
+                embedding = res[0]["embedding"]
+                # print("embeding", embedding)
+                # print("\nperson ident_bef", person_ident, "")
+                person_ident.append(embedding)
+                # print("\nperson ident", person_ident, "\n")
+            else:
+                res = represent(os.path.join(db_recognition_path, name_generator(
+                    k, i)), model_name=model_name, detector_backend="skip")
+                embedding = res[0]["embedding"]
+                for j in range(3):
+                    # print(embedding)
+                    # print(person_ident)
+                    dist = distance.findCosineDistance(
+                        person_ident[j], embedding)
+                    if dist < threshold:
+                        print("img", i, "match", j, "\tdist", dist)
+                    else:
+                        print("no_match")
     print("Total time", time.time() - start_recognition_time)
