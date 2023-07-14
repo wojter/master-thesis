8c04532723d347a9e6984407afcf949fdd9166d6
wojter <wojtektro2@gmail.com>
2023-07-14 20:50:43 +0200

    Add read img, apply noise, save

--- facelytics/generate_noise.py
+++ facelytics/generate_noise.py
@@ -8,11 +8,13 @@ import argparse
 
 noise_types = {
     "gaussian",
-    "salt_vs_pepper",
+    "s_v_p",
     "poisson",
     "speckle",
     "gaussian_blur"
 }
+
+source_db_path = "CelebA/img_prepared"
 
 
 def args_inpust():
@@ -107,9 +109,48 @@ def show_noise_examples(file_path, img_name):
     plt.show()
 
 
+def get_list_all_img(dir_path):
+    list_files = []
+    for path in os.scandir(dir_path):
+        if path.is_file():
+            list_files.append(path.name)
+    return list_files
+
+
+def read_img(img_name):
+    img_path = os.path.join(source_db_path, img_name)
+    source_img = skimage.io.imread(img_path)
+    return source_img
+
+
+def write_img(img, img_name, dest_path):
+    dest_path = os.path.join(dest_path, img_name)
+    skimage.io.imsave(dest_path, img)
+
+
+def create_dest_dir(selected_noise_type, param=None):
+    dest_path = ""
+    if param is None:
+        dest_path = f"db_imgs_{selected_noise_type}"
+    else:
+        dest_path = f"db_imgs_{selected_noise_type}_{param:.02f}"
+        dest_path = dest_path.replace(".", "_")
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+    return dest_path
+
 if __name__ == "__main__":
     args = args_inpust()
     source_dir, result_dir, num_img_as_ref, num_img_of_ident, num_ident, selected_noise = args_parser(
         args)
     if selected_noise not in noise_types:
         raise ValueError("This noise is not implemented")
+
+    list_imgs = get_list_all_img("CelebA/img_db_recognition")
+
+    if selected_noise == "gaussian":
+        for var in np.arange(0.5, 5.5, 0.5):
+            result_dir = create_dest_dir(selected_noise, var)
+            for img in list_imgs:
+                result_img = gaussian_noise_generator(read_img(img))
+                write_img(result_img, img,)
