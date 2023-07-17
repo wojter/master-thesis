ea3e006a09dcb1f1a393b77e10c1f6d3b566a4e4
wojter <wojtektro2@gmail.com>
2023-07-18 00:41:17 +0200

    move files fix

--- facelytics/move_imgs.py
+++ facelytics/move_imgs.py
@@ -5,16 +5,22 @@ from tqdm import tqdm
 from common import img_name_generator
 
 
-db_recognition_path = os.path.join("CelebA", "img_prepared")
-db_rec = os.path.join("CelebA", "img_db")
+source_db_path = os.path.join("CelebA", "img_prepared")
+ident_db = os.path.join("CelebA", "img_db")
 
 
 def main():
+    if not os.path.isdir(source_db_path):
+        raise NotADirectoryError("Source directory not exsist")
+
+    if not os.path.isdir(ident_db):
+        os.mkdir(source_db_path)
+    
     for i in tqdm(range(1, 1000 + 1)):
         for j in range(1, 3 + 1):
             shutil.move(
-                os.path.join(db_recognition_path, img_name_generator(i, j)),
-                os.path.join(db_rec, img_name_generator(i, j)),
+                os.path.join(source_db_path, img_name_generator(i, j)),
+                os.path.join(ident_db, img_name_generator(i, j)),
             )
 
 
