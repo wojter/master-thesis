a8a456978a9fe3b3454560609abc9cb9526ee271
wojter <wojtektro2@gmail.com>
2023-07-17 23:40:16 +0200

    Fix paths, tqdm import, code refactor

--- facelytics/move_imgs.py
+++ facelytics/move_imgs.py
@@ -1,15 +1,22 @@
 import shutil
 import os
-import tqdm
+from tqdm import tqdm
 
 from common import img_name_generator
 
 
-db_recognition_path = "CelebA\img_prepared"
-db_rec = "CelebA\img_db"
-for i in tqdm(range(1, 1000 + 1)):
-    for j in range(1, 3 + 1):
-        shutil.move(
-            os.path.join(db_recognition_path, img_name_generator(i, j)),
-            os.path.join(db_rec, img_name_generator(i, j)),
-        )
+db_recognition_path = os.path.join("CelebA", "img_prepared")
+db_rec = os.path.join("CelebA", "img_db")
+
+
+def main():
+    for i in tqdm(range(1, 1000 + 1)):
+        for j in range(1, 3 + 1):
+            shutil.move(
+                os.path.join(db_recognition_path, img_name_generator(i, j)),
+                os.path.join(db_rec, img_name_generator(i, j)),
+            )
+
+
+if __name__ == "__main__":
+    main()
