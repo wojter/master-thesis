41708084b0655f439d712e576d00bdd90d8e95ae
wojter <wojtektro2@gmail.com>
2023-07-22 11:20:01 +0200

    Code refactor, add progressbar

--- facelytics/test_model.py
+++ facelytics/test_model.py
@@ -1,8 +1,12 @@
 import os
 import time
 import random
+from tqdm import tqdm
+import argparse
+
 from deepface.DeepFace import represent, build_model
 from deepface.commons import distance, functions
+
 
 import matplotlib.pyplot as plt
 from deepface import DeepFace
@@ -43,43 +47,47 @@ selected_model = "Facenet"
 positives_distances = []
 negatives_distances = []
 
-model = build_model(selected_model)
-target_size = functions.find_target_size(model_name=selected_model)
+def main():
+    model = build_model(selected_model)
+    target_size = functions.find_target_size(model_name=selected_model)
 
-for ident in range(1, NUM_IDENT_TO_TEST + 1):
-    for i in range(1, 4):
-        img_obj = functions.extract_faces(
-            os.path.join(db_identity, img_name_generator(ident, i)),
-            target_size=target_size,
-            detector_backend="skip",
-        )
-        res = represent(
-            img_obj[0][0], model_name=selected_model, detector_backend="skip"
-        )
-        embedding_org = res[0]["embedding"]
-        for j in range(4, 10):
+    for ident in tqdm(range(1, NUM_IDENT_TO_TEST + 1)):
+        for i in range(1, 4):
             img_obj = functions.extract_faces(
-                os.path.join(db_to_test, img_name_generator(ident, j)),
+                os.path.join(db_identity, img_name_generator(ident, i)),
                 target_size=target_size,
                 detector_backend="skip",
             )
             res = represent(
                 img_obj[0][0], model_name=selected_model, detector_backend="skip"
             )
-            embedding = res[0]["embedding"]
-            dist = distance.findEuclideanDistance(
-                distance.l2_normalize(embedding_org), distance.l2_normalize(embedding)
-            )
-            dist_rounded = round(dist, 4)
-            positives_distances.append(dist_rounded)
+            embedding_org = res[0]["embedding"]
+            for j in range(4, 10):
+                img_obj = functions.extract_faces(
+                    os.path.join(db_to_test, img_name_generator(ident, j)),
+                    target_size=target_size,
+                    detector_backend="skip",
+                )
+                res = represent(
+                    img_obj[0][0], model_name=selected_model, detector_backend="skip"
+                )
+                embedding = res[0]["embedding"]
+                dist = distance.findEuclideanDistance(
+                    distance.l2_normalize(embedding_org), distance.l2_normalize(embedding)
+                )
+                dist_rounded = round(dist, 4)
+                positives_distances.append(dist_rounded)
 
-df = pd.DataFrame(positives_distances)
-print(df.mean())
-print(df.std())
+    df = pd.DataFrame(positives_distances)
+    print(df.mean())
+    print(df.std())
 
-plt.figure()
-df.plot.kde()
-plt.show()
+    plt.figure()
+    df.plot.kde()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
 # def generat_pair():
 #     j = random.randint(1, NUM_IDENT_TO_TEST)
 #     k = random.randint(1, 3)
