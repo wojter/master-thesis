690f1a13e2de198f805d778674d33d286710d0de
wojter <wojtektro2@gmail.com>
2023-07-25 01:57:46 +0200

    add call function

---
+++ facelytics/call.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+
+from commons import models
+
+model_name = "DeepFace"
+img_dir = "db_imgs_gaussian_"
+
+
+if model_name not in models:
+    raise ValueError("Wrong model name")
+
+
+img_dir = "db_imgs_gaussian_"
+for i in np.arange(0.05, 0.3, 0.05):
+    command = (
+        "python3 facelytics/test_model.py -m "
+        + model_name
+        + " -f "
+        + img_dir
+        + "{:.2f}".format(i).replace(".", "_")
+    )
+    try:
+        os.system(command)
+    except:
+        pass
